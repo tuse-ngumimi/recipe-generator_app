cd4afd4fce447d02b6f603360aecac77496cca92
Ngumimi Tuse <tusengumimi21@gmail.com>
2026-01-28 11:30:10 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const CookdApp());
+}
+
+class CookdApp extends StatelessWidget {
+  const CookdApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "COOK'd",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFFEF7930),
+          primary: const Color(0xFFEF7930),
+          secondary: const Color(0xFFFFF4E6),
+          surface: const Color(0xFFFFF4E6),
+        ),
+        scaffoldBackgroundColor: const Color(0xFFFFFBF5),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFFEF7930),
+          foregroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color(0xFFFF8C42),
+          foregroundColor: Colors.white,
+        ),
+        cardTheme: CardTheme(
+          color: Colors.white,
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFFFF8C42)),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFFFFD4B8)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFFEF7930), width: 2),
+          ),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
